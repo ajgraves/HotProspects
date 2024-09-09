123b7558a82cb900ea358f1895eeb49536a3270c
Aaron Graves <ajgraves@ajgraves.com>
2024-09-09 14:41:34 -0500

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Aaron Graves on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
