04efec8da005746b815875086bf0c0120d8e9b11
Aaron Graves <ajgraves@ajgraves.com>
2024-09-11 10:07:00 -0500

    Reset the app to start actual work

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -5,20 +5,11 @@
 //  Created by Aaron Graves on 9/9/24.
 //
 
-import SamplePackage
 import SwiftUI
 
 struct ContentView: View {
-    let possibleNumbers = 1...60
-    
-    var results: String {
-        let selected = possibleNumbers.random(7).sorted()
-        let strings = selected.map(String.init)
-        return strings.formatted()
-    }
-    
     var body: some View {
-        Text(results)
+        Text("Hello")
     }
 }
 
